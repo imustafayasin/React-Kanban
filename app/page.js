c3e249436e09902f4129a1e28922383bec4dcc12
imustafayasin <imustafayasin@gmail.com>
2023-02-25 23:42:07 +0300

    Move modals to base index

--- app/page.js
+++ app/page.js
@@ -2,24 +2,15 @@
 import Sidebar from "../components/Sidebar";
 import ContentHeader from "../components/ContentHeader";
 import Columns from "../components/Columns";
-
-import AddBoardModal from "../components/modals/AddBoardModal";
-import UpdateBoardModal from "../components/modals/UpdateBoardModal";
-import AddTaskModal from "../components/modals/AddTaskModal";
-import UpdateTaskModal from "../components/modals/UpdateTaskModal";
+import Modals from "../components/modals";
 
 import { store } from "../store/index";
 import { Provider } from "react-redux";
 
 export default function Home() {
-
-
   return (
     <Provider store={store}>
-      <AddBoardModal />
-      <UpdateBoardModal />
-      <AddTaskModal />
-      <UpdateTaskModal />
+      <Modals />
       <div className="mx-auto flex flex-row h-full overflow-hidden w-full	relative">
         <Sidebar />
         <main className="w-full">
---
+++ components/modals/index.js
@@ -0,0 +1,14 @@
+import AddBoardModal from "./AddBoardModal";
+import UpdateBoardModal from "./UpdateBoardModal";
+import AddTaskModal from "./AddTaskModal";
+import UpdateTaskModal from "./UpdateTaskModal";
+export default function Modals() {
+  return (
+    <>
+      <AddBoardModal />
+      <UpdateBoardModal />
+      <AddTaskModal />
+      <UpdateTaskModal />
+    </>
+  );
+}
