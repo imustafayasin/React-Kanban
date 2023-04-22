97b2acf42c3aaba923c328e09a60a1807ccbe3c4
imustafayasin <imustafayasin@gmail.com>
2023-04-22 09:06:46 +0300

    Add style to email login button

--- services/email/sendVerificationEmail.js
+++ services/email/sendVerificationEmail.js
@@ -8,10 +8,12 @@ export default async function sendVerificationEmail(to, userId) {
     //todo refactor here
     await saveUserToken(userId, token);
 
+    const style_string =
+      "text-decoration: none; margin-block: 40px; display: inline-flex; margin-inline: auto; text-align: center; line-height: 40px; font-size: 16px; background-color: #7f56d9; color: #fff; padding: 6px 17px; border-radius: 8px; border: 1px solid #7f56d9; box-shadow: 2px 4px 6px #0002;";
     let emailResponse = await sendEmail(
       to,
       "Login your KANBAN Account",
-      `<a href="${process.env.DEVELOPMENT_URL}/register/verification?token=${token}">Login your account</a>`
+      `<a style="${style_string}" href="${process.env.DEVELOPMENT_URL}/register/verification?token=${token}">Login your account</a>`
     );
 
     return { success: true };
